535dbac2b84ec990976c3eadf98846e2c8f3c75a
brandon1804 <18044455@rp.edu.sg>
2021-07-29 12:58:16 +0800

    Modified Task class

--- app/src/main/java/com/example/p11taskmanager/Task.java
+++ app/src/main/java/com/example/p11taskmanager/Task.java
@@ -27,6 +27,6 @@ public class Task implements Serializable {
 
     @Override
     public String toString() {
-        return id + name +"\n" + description;
+        return (id + 1) + " " + name + "\n" + description;
     }
 }
